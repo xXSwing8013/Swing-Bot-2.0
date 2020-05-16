bbcfa9eadd52d6e8ab1238fe8fc965bb9f38713d
xXSwing8013 <65372262+xXSwing8013@users.noreply.github.com>
2020-05-16 12:08:49 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,58 +1,61 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const token1 = 'NzEwODc3MDk4NTc0ODcyNjA3.Xr9xTg.sxXTQgGv4w4hLTF9xVHqvwypwCo';
+const token = 'NzA3Nzk5MTMyODE0NzA0Njkx.XrcFCw.pv7CmLNtms7eW_p2tnlWodNQYks';
 
-const PREFIX = '+';
+const PREFIX = '$'
 
-client.on('ready', () =>{
-    console.log('This bot is online!');
-})
+client.on('ready', () => {
 
-client.on('message', message=>{
+  switch(args[0]) {}
+   client.on('message', message =>{
 
-    let args = message.content.substring(PREFIX.length).split(" ");
-
-    switch(args[0]){
-        case'test':
-            message.reply('Bot is online!');
-            break;
-        case'cmd':
-            message.reply('https://pastebin.com/nLb21Ewf full command list');
-            break;
-        case'patreon':
-            message.reply('https://www.patreon.com/xXSwing');
-            break;
+      let args = message.content.substring(PREFIX.length.split(""));
+     
+      switch(args[0]){
+        case 'help':
+          message.reply('pastebin https://pastebin.com/Gjr8BnAz');
+        break;
+        case'info':
+          message.reply('Made by xXSwing™ 5/7/2020');
+        break;
         case 'author':
-            message.reply('xXSwing#8013')
-            break;
-        case 'jesmar':
-            message.reply('https://www.roblox.com/users/247525563/profile');
-            break;
-        case 'ttv':  
-            message.reply('https://www.roblox.com/users/1075239751/profile');
-            break;   
-        case'achro':
-            message.reply('https://www.roblox.com/users/377373465/profile');
-            break;
-        case'ally':
-            message.reply('https://www.roblox.com/groups/5608197/Dead-End#!/about');
-            break;
-        case'tne':
-            message.reply('https://www.roblox.com/groups/6253281/The-New-Era-Da-Hood#!/about');
-            break;
-        case 'ps':
-            message.reply('https://www.roblox.com/games/2788229376/Da-Hood?privateServerLinkCode=stsCE3213sG8nDCITbtOQVXMH_yqo4yJ');
-            break;
-        case'vipers':
-            message.reply('https://www.roblox.com/groups/5055250/South-Side-Vipers');
-            break;
-        case'yt':
-            message.reply('https://www.youtube.com/channel/UCMpwstFhQJemtNU0sH_2PfQ?view_as=subscriber');
-            break;
-    
-    }
-
-})
-
-client.login(process.env.BOT_TOKEN);
+          message.reply('xXSwing#8013');
+        break;
+        case 'failure':
+          message.reply('Me_Failrue 🍆 is hard.');
+        break;
+        case 'devilz':
+          message.reply('https://www.roblox.com/groups/5077356/Devilsz#!/about');
+        break;
+        case 'angelz':
+          message.reply('https://www.roblox.com/groups/5488398/Deadly-AngeLZ#!/about');
+        break;
+        case 'chanekes':
+          message.reply('https://www.roblox.com/groups/5036877/Chanekes#!/about');
+        break;
+        case 'eamily':
+          message.reply('eamily is a 👑!');
+        break;
+        case 'guest':
+          message.reply('Guest is a 🐐!');
+        break;
+        case 'ninito':
+          message.reply('ninito is the best Da Hood leader!');
+        break;
+        case 'juan':
+          message.reply('Juan es mi amigo');
+        break;
+        case 'booze':
+          message.reply('Take a sip :tropic_drink: and feel the greatest laugh of all time!');
+        break;
+        case 'enforce':
+          message.reply('🚨 FBI OPEN UP');
+        break;
+        case 'aidin':
+          message.reply('Edgenuity u watch videos and then they give u a quiz - Aidin');
+        break;
+      }  
+   })
+});
+client.login(token);
